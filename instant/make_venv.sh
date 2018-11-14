c61d1f559b1d835e2cd814f156cd5c1fe3f4b248
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2018-11-14 11:32:39 +0100

    Improve Instant Makefile

--- instant/make_venv.sh
+++ instant/make_venv.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
+PYTHON_EXEC="$(which python3.7)"
+if [ -z "${PYTHON_EXEC}" ]; then
+    PYTHON_EXEC="$(which python3.6)"
+fi
+if [ -z "${PYTHON_EXEC}" ]; then
+    echo "Could not find Python 3.7 or 3.6 executable"
+    return 1
+fi
 
-python3 -m venv ./py3_venv && \
-source py3_venv/bin/activate && \
-pip3 install -r requirements.txt
+set -e
+
+"${PYTHON_EXEC}" -m venv ./py3_venv
+./py3_venv/bin/pip3 install -r requirements.txt
